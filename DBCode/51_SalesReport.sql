364b1d1ef12655c39352efa21af3b1c052d4569e
Steve <way0utwest@hotmail.com>
2022-10-17 17:07:56 -0400

    provision demo updates

--- DBCode/51_SalesReport.sql
+++ DBCode/51_SalesReport.sql
@@ -1,4 +1,14 @@
--- Procedure already in Prod
+/*
+Data Community Summit Precon
+
+51 - Test Data Management
+
+We need to have some production data in development, in  safe manner
+
+Copyright 2022 Redgate Software
+*/
+
+-- This procedure is already in Production
 CREATE OR ALTER PROCEDURE SalesReport
  @StartDate datetime
  , @EndDate datetime
@@ -23,15 +33,16 @@ BEGIN
 END
 
 GO
--- Ticket filed in production about bad order data
--- DBA Investigate in production
--- Devs SHOULD HAVE NO ACCESS to Production
+
+-- Someone files a ticket about a problem in production
+-- A DBA investigates in production with this script.
+-- Devs have NO access to production
 USE Westwind
 GO
 EXEC SalesReport '2022/01/01', '2022/12/31', 1
 
--- Zero dollar sale
--- get from above
+-- There is a zero dollar sale (among other issues
+-- get OrderIDs from above
 SELECT * FROM dbo.[Order Details] AS od WHERE od.OrderID IN (26482,
 26301,
 13073
@@ -41,7 +52,7 @@ SELECT * FROM dbo.[Order Details] AS od WHERE od.OrderID IN (26482,
 -- Nothing should be higher than .4
 SELECT * FROM dbo.[Order Details] AS od WHERE od.Discount > .8
 
--- Check the others
+-- Check the other orders for more issues
 -- Find more problems
 SELECT o.*
 FROM dbo.[Order Details] AS od
@@ -50,8 +61,7 @@ FROM dbo.[Order Details] AS od
 WHERE od.Discount > .8;
 
 
--- Check in development
--- Can a Dev replicate this problem
+-- Let's check in development
 use Westwind_1_Dev
 GO
 
@@ -61,29 +71,35 @@ SELECT * FROM dbo.[Order Details] AS od WHERE od.Discount > .8
 
 
 -- Check the constraint
+-- Westwind_1_Dev, Tables, Constraints
+-- There is no CK_ constraint on discount
+-- Someone deleted it.
+
 
 
 
 
 -- No restrictions on Discount
--- we had a malicious employee
+-- we had a problem, a mistake, a hack, or a malicious employee
 -- We need better data in dev
 
 
 -- Get updated dev environment from TDM/Data Virtualization (Cloning tech)
 -- Do this safely
-
-
 -- create clone
--- check data
--- Westwind
+
+
+
+
+
 /*
+-- Run this for production
 use westwind
 go
 SELECT * FROM dbo.[Order Details] AS od WHERE od.Discount > .8
 SELECT * from employees
 
--- Vertical tab
+-- New Vertical tab in SSMS (Window| Vertical Tab Group
 use westwind_1_Beca
 go
 SELECT * FROM dbo.[Order Details] AS od WHERE od.Discount > .8
@@ -94,3 +110,4 @@ SELECT * from employees
 
 -- We also need a constraint, but we need to talk to business people
 -- about how to deal with existing data and decide what to change.
+-- alter table Order Details add constraint CK_OD_Discount for discount < .8 and discount >= 0
