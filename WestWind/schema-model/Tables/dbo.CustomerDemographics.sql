1181bf2bf1b31561a44efd3a18a423c73f47d734
Grant Fritchey <grant@scarydba.com>
2022-11-14 17:23:13 -0600

    Clean up from test

--- WestWind/schema-model/Tables/dbo.CustomerDemographics.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[CustomerDemographics]
-(
-[CustomerTypeID] [nchar] (10) NOT NULL,
-[CustomerDesc] [ntext] NULL,
-[nationality] [nvarchar] (20) NULL
-)
-GO
-ALTER TABLE [dbo].[CustomerDemographics] ADD CONSTRAINT [PK_CustomerDemographics] PRIMARY KEY NONCLUSTERED ([CustomerTypeID])
-GO
--- WestWind/schema-model/Tables/dbo.Summit2022.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Summit2022]
-(
-[Summit2022ID] [int] NOT NULL IDENTITY(1, 1),
-[QuoteBy] [varchar] (50) NULL,
-[Quote] [varchar] (500) NULL
-)
-GO
-ALTER TABLE [dbo].[Summit2022] ADD CONSTRAINT [Summit2022PK] PRIMARY KEY CLUSTERED ([Summit2022ID])
-GO
