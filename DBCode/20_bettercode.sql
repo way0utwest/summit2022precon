683a4178f6825fe103010362cd3cb9fdb6d4e63c
Steve <way0utwest@hotmail.com>
2022-11-15 11:08:08 -0800

    dfgdfg

--- DBCode/20_bettercode.sql
+++ DBCode/20_bettercode.sql
@@ -55,3 +55,15 @@ PictureDesc varchar(200),
 PictureURL varchar(300)
 */
 
+CREATE TABLE dbo.Summit2022Pix (
+ Summit2022PixID INT IDENTITY(1,1) NOT NULL CONSTRAINT Summit2022PixPK PRIMARY KEY,
+PictureDesc varchar(200),
+PictureURL varchar(300)
+)
+GO
+GRANT SELECT, INSERT, UPDATE, DELETE ON dbo.Summit2022Pix TO WebUsers
+
+SELECT top 10
+ *
+ FROM dbo.Summit2022Pix
+ 
